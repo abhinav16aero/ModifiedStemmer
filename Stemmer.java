5f8b965cb420290c42e82a05107ed8a8ed815593
Abhinav <114936870+abhinav16aero@users.noreply.github.com>
2024-06-17 08:14:17 +0000

    modified:   Stemmer.class 	modified:   Stemmer.java

--- Stemmer.java
+++ Stemmer.java
@@ -354,62 +354,45 @@
       * Test program for demonstrating the Stemmer. It reads text from a
       * string, stems each word, and writes the result to standard output.
       */
-     public static void main(String[] args)
-       {
-          char[] w = new char[501];
-          Stemmer s = new Stemmer();
-          for (int i = 0; i < args.length; i++)
-          try
-          {
-             FileInputStream in = new FileInputStream(args[i]);
 
-             try
-             { while(true)
 
-               {  int ch = in.read();
-                  if (Character.isLetter((char) ch))
-                  {
-                     int j = 0;
-                     while(true)
-                     {  ch = Character.toLowerCase((char) ch);
-                        w[j] = (char) ch;
-                        if (j < 500) j++;
-                        ch = in.read();
-                        if (!Character.isLetter((char) ch))
-                        {
-                           /* to test add(char ch) */
-                           for (int c = 0; c < j; c++) s.add(w[c]);
 
-                           /* or, to test add(char[] w, int j) */
-                           /* s.add(w, j); */
-
-                           s.stem();
-                           {  String u;
-
-                              /* and now, to test toString() : */
-                              u = s.toString();
-
-                              /* to test getResultBuffer(), getResultLength() : */
-                              /* u = new String(s.getResultBuffer(), 0, s.getResultLength()); */
-
-                              System.out.print(u);
-                           }
-                           break;
-                        }
-                     }
-                  }
-                  if (ch < 0) break;
-                  System.out.print((char)ch);
-               }
-             }
-             catch (IOException e)
-             {  System.out.println("Error In Reading " + args[i]);
-                break;
-             }
-          }
-          catch (FileNotFoundException e)
-          {  System.out.println("File " + args[i] + " Not Found");
-             break;
-          }
-       }
-    }
+    public static String stemInputString(String input) {
+        Stemmer s = new Stemmer();
+        char[] wordArray = input.toCharArray();
+        int length = wordArray.length;
+        StringBuilder stemmedText = new StringBuilder();
+  
+        for (int i = 0; i < length; i++) {
+           int ch = wordArray[i];
+           if (Character.isLetter((char) ch)) {
+              int j = 0;
+              while (i < length && Character.isLetter((char) ch)) {
+                 ch = Character.toLowerCase((char) ch);
+                 s.add((char) ch);
+                 i++;
+                 if (i < length) {
+                    ch = wordArray[i];
+                 }
+              }
+              s.stem();
+              stemmedText.append(s.toString()).append(" ");
+              i--;
+           } else {
+              stemmedText.append((char) ch);
+           }
+        }
+        return stemmedText.toString().trim();
+     }
+  
+     public static void main(String[] args) {
+        if (args.length == 0) {
+           System.out.println("Please provide an input string as a command-line argument.");
+           return;
+        }
+  
+        String input = args[0]; // Read the first command-line argument
+        String result = Stemmer.stemInputString(input);
+        System.out.println("Stemmed Result: " + result);
+     }
+  }
